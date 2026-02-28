0feac51bff61992bb15539278f3b5b0109c0e8dd
mansouriali263-afk <mansouriali263@gmail.com>
2026-02-28 13:46:48 +0100

    Update realfinance.py

--- realfinance.py
+++ realfinance.py
@@ -32,7 +32,7 @@ class HealthHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b"<html><body><h1>🤖 REFi Bot is running!</h1></body></html>")
+        self.wfile.write(b"<html><body><h1>REFi Bot is running!</h1></body></html>")
     
     def do_HEAD(self):
         self.send_response(200)
